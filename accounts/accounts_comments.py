95145e8788aef908f0da016875844a4db37ea359
jamestha3d <jamesikediashi@gmail.com>
2023-06-08 22:08:19 -0400

    complex filter with Count, Exists, OuterRef)

--- accounts/accounts_comments.py
+++ accounts/accounts_comments.py
@@ -1,1 +1,10 @@
 #Test file
+
+
+"""
+active_users = Usr.objects.annotate(has_subscription=Exists(Subscription.objects.filter(userguid=OuterRef('pk')))).filter(has_subscription=True,assessments__summarydatetime__gt=date_threshold).distinct().values('guid', 'username', 'email')
+
+active_users = Usr.objects.annotate(subscriptions_count=Count('subscriptions')).filter(subscription_count__gte=1,assessments__summarydatetime__gt=date_threshold).distinct().values('guid', 'email')
+
+Same query implemented differently
+"""
